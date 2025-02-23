2487eb3d5a5340823016f38b2b689c9ae55230a1
sikandar <sikandarcode2024@gmail.com>
2025-02-23 12:22:03 +0530

    Comitted Multiply

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+package com.tut.dsa;
+
+public class Hello {
+    public static void main(String args[])
+    {
+         int a=5,int b=6;
+        System.out.println("Hello Sikandar"+a*b);
+    }
+    
+}
